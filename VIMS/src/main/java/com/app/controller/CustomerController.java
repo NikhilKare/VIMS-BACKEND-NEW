840ceafcf7b4995441c63f0a89b6bca4ef54860c
Admin <pathansam045@gmail.com>
2022-09-16 22:15:12 +0530

    update

--- VIMS/src/main/java/com/app/controller/CustomerController.java
+++ VIMS/src/main/java/com/app/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.app.services.ICustomerService;
 import com.app.utils.Response;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/customers/{id}")
 public class CustomerController {
 
