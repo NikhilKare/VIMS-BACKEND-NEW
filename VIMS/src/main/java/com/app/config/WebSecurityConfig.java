294ab77a2ce1d6c8f119bc231fd9c4c2bd7bf455
NikhilKare <109783223+NikhilKare@users.noreply.github.com>
2022-09-18 16:36:47 +0530

    UpdatedBackend

--- VIMS/src/main/java/com/app/config/WebSecurityConfig.java
+++ VIMS/src/main/java/com/app/config/WebSecurityConfig.java
@@ -44,7 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
 		.authorizeRequests()
-		.antMatchers("/user").hasAnyRole("USER","ADMIN")
+		.antMatchers("/user/**").permitAll()/*hasAnyRole("USER","ADMIN")*/
 		.antMatchers("/customers").hasRole("CUSTOMER")
 		.antMatchers("/admin").hasRole("ADMIN")
 		.antMatchers("/api/home/**").permitAll() //enabling global access to all urls starting with /api/auth
